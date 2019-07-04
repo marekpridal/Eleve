3a65c4189343aa7413182ae41134c41705bb512c
Marek Pridal <marek@marekpridal.eu>
2019-07-04 18:42:30 +0200

    Encapsulate content view of detail into scroll view

--- Eleve/ViewControllers/ElevatorDetailView.swift
+++ Eleve/ViewControllers/ElevatorDetailView.swift
@@ -16,7 +16,7 @@ final class ElevatorDetailView : View {
     }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
+        VStack(alignment: .leading) {
             HStack() {
                 VStack(alignment: .leading) {
                     Text(viewModel.elevator.name)
@@ -35,52 +35,61 @@ final class ElevatorDetailView : View {
                 .padding()
             }
             Divider()
-            HStack(alignment: VerticalAlignment.bottom) {
-                Button(action: { [weak self] in
-                    self?.viewModel.favorite()
-                }) {
-                    CircleView(icon: Image(systemName: "star.fill"), color: Color("Orange"))
-                }
-                Spacer()
-                Group {
-                    Button(action: { [weak self] in
-                        guard let self = self else { return }
-                        self.viewModel.delegate?.navigate(elevator: self.viewModel.elevator)
-                    }, label: {
-                        RoundedView(title: "NAVIGATE", color: Color("Blue"), icon: Image(systemName: "arrow.up"))
-                    })
-                }
-                Spacer()
-                Button(action: { [weak self] in
-                    guard let self = self else { return }
-                    self.viewModel.delegate?.report(elevator: self.viewModel.elevator)
-                }, label: {
-                    RoundedView(title: "REPORT", color: Color("Red"), icon: Image(systemName: "arrow.up"))
-                })
-            }
-            .padding(.trailing, 20)
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(1...50) { element in
-                        Button(action: {
-                            //self?.viewModel.delegate?.showImageDetail(element.image)
+            ScrollView {
+                VStack(spacing: 15) {
+                    HStack(alignment: VerticalAlignment.bottom) {
+                        Button(action: { [weak self] in
+                            self?.viewModel.favorite()
                         }) {
-                            Image(systemName: "selection.pin.in.out")
-                                .frame(width: 139, height: 84, alignment: Alignment.center)
-                                .background(Rectangle().fill(Color("Blue")).cornerRadius(6))
+                            CircleView(icon: Image(systemName: "star.fill"), color: Color("Orange"))
                         }
+                        Spacer()
+                        Group {
+                            Button(action: { [weak self] in
+                                guard let self = self else { return }
+                                self.viewModel.delegate?.navigate(elevator: self.viewModel.elevator)
+                                }, label: {
+                                    RoundedView(title: "NAVIGATE", color: Color("Blue"), icon: Image(systemName: "arrow.up"))
+                            })
+                        }
+                        Spacer()
+                        Button(action: { [weak self] in
+                            guard let self = self else { return }
+                            self.viewModel.delegate?.report(elevator: self.viewModel.elevator)
+                            }, label: {
+                                RoundedView(title: "REPORT", color: Color("Red"), icon: Image(systemName: "arrow.up"))
+                        })
+                    }
+                    .padding(.trailing, 20)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(1...50) { element in
+                                Button(action: {
+                                    //self?.viewModel.delegate?.showImageDetail(element.image)
+                                }) {
+                                    Image(systemName: "selection.pin.in.out")
+                                        .frame(width: 139, height: 84, alignment: Alignment.center)
+                                        .background(Rectangle().fill(Color("Blue")).cornerRadius(6))
+                                }
+                            }
+                        }
+                    }
+                    VStack(alignment: .leading) {
+                        Text("STATES")
+                            .font(.system(size: 16))
+                            .fontWeight(.semibold)
+                        ForEach(1...50) { element in
+                            ElevatorStateView(elevatorState: ElevatorStateView.ElevatorState(working: Bool.random(), direction: "Z / Na nástupiště", lastUpdate: TimeInterval.random(in: 1000...10_000_000)))
+                                .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 20))
+                            Divider()
+                                .padding(.leading, 35)
+                        }
+                        Text("INFORMATION")
+                            .font(.system(size: 16))
+                            .fontWeight(.semibold)
                     }
                 }
             }
-            Text("STATES")
-                .font(.system(size: 16))
-                .fontWeight(.semibold)
-                .padding(.bottom, -10)
-            List(1...50) { element in
-                ElevatorStateView(elevatorState: ElevatorStateView.ElevatorState(working: Bool.random(), direction: "Z / Na nástupiště", lastUpdate: TimeInterval.random(in: 1000...10_000_000)))
-                .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
-            }
-            .padding(EdgeInsets(top: 0, leading: -15, bottom: 0, trailing: 0))
         }
         .padding(EdgeInsets(top: 25, leading: 20, bottom: 0, trailing: 0))
     }

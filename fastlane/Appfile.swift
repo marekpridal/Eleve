b02fb57ce134c92f517dd0b5e76b21b2a0bbd3c2
Marek Pridal <marek@marekpridal.eu>
2020-02-26 22:44:47 +0100

    Add fastlane

---
+++ fastlane/Appfile.swift
@@ -0,0 +1,9 @@
+var appIdentifier: String { return "eu.marekpridal.Eleve" } // The bundle identifier of your app
+var appleID: String { return "marek@marekpridal.eu" } // Your Apple email address
+
+var itcTeam: String? { return "118755174" } // App Store Connect Team ID
+var teamID: String { return "JV7TARSQ32" } // Apple Developer Portal Team ID
+
+
+// For more information about the Appfile, see:
+//     https://docs.fastlane.tools/advanced/#appfile

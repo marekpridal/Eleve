6c4b1159c6ea985aa64c17bb831799b275fe77c2
Marek Pridal <marek@marekpridal.eu>
2020-02-26 22:52:21 +0100

    Update Fastfile

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -11,8 +11,27 @@ import Foundation
 class Fastfile: LaneFile {
 	func betaLane() {
 	desc("Push a new beta build to TestFlight")
-		incrementBuildNumber(xcodeproj: "Eleve.xcodeproj")
+		let newBuildNumber = incrementBuildNumber(xcodeproj: "Eleve.xcodeproj")
+        let versionNumber = getVersionNumber(xcodeproj: "Eleve.xcodeproj")
+        let changeLog = changelogFromGitCommits(between: "\(lastGitTag()),HEAD")
+
 		buildApp(scheme: "Eleve")
-		uploadToTestflight(username: "marek@marekpridal.eu")
+
+        
+        // Add to git, commit and push
+        gitAdd()
+        let commitMessage =
+        """
+        New build for TestFlight ✈️ \(versionNumber)b\(newBuildNumber)
+        
+        Changelog:
+        \(changeLog)
+        """
+        sh(command: "git commit -S -m \"\(commitMessage)\"")
+        pushToGitRemote(localBranch: gitBranch())
+        addGitTag(buildNumber: "\(versionNumber)b\(newBuildNumber)")
+        pushToGitRemote(localBranch: gitBranch())
+        
+		uploadToTestflight(username: "marek@marekpridal.eu", teamId: "118755174")
 	}
 }
